e58bf72a84d02898107810a4a64e5541f0eec2bd
lebanx <47849963+lebanx@users.noreply.github.com>
2025-08-06 08:46:18 +0800

    Add files via upload

---
+++ build/psr/http-client/src/ClientExceptionInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @license MIT
+ *
+ * Modified by __root__ on 09-September-2024 using {@see https://github.com/BrianHenryIE/strauss}.
+ */
+
+namespace Wenprise\Wechatpay\Psr\Http\Client;
+
+/**
+ * Every HTTP client related exception MUST implement this interface.
+ */
+interface ClientExceptionInterface extends \Throwable
+{
+}
