7ff0df551c90e940361fc5d06230caa98eafad62
lebanx <47849963+lebanx@users.noreply.github.com>
2025-08-06 08:42:11 +0800

    Add files via upload

---
+++ build/moneyphp/money/src/Exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @license MIT
+ *
+ * Modified by __root__ on 09-September-2024 using {@see https://github.com/BrianHenryIE/strauss}.
+ */
+
+namespace Wenprise\Wechatpay\Money;
+
+/**
+ * Common interface for all exceptions thrown by this library.
+ *
+ * @author Frederik Bosch <f.bosch@genkgo.nl>
+ */
+interface Exception
+{
+}
