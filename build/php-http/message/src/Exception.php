13f1eea60edc80206cfc2283cb1516dae2cfb420
lebanx <47849963+lebanx@users.noreply.github.com>
2025-08-06 08:45:11 +0800

    Add files via upload

---
+++ build/php-http/message/src/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @license MIT
+ *
+ * Modified by __root__ on 09-September-2024 using {@see https://github.com/BrianHenryIE/strauss}.
+ */
+
+namespace Wenprise\Wechatpay\Http\Message;
+
+/**
+ * An interface implemented by all HTTP message related exceptions.
+ */
+interface Exception
+{
+}
